044d9c3e6d3442e87e353693f02f41d555b02350
Jingxi Chen <42823098+codingrex@users.noreply.github.com>
2021-02-08 22:45:21 -0500

    Create supervised network

---
+++ supv_agent.py
@@ -0,0 +1,34 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchsummary import summary
+import torch.optim as optim
+from torch.distributions import Categorical
+from statistics import mean
+from torch.utils.tensorboard import SummaryWriter
+import random
+
+class SupvNet(nn.Module):
+    def __init__(self):
+        super(SupvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(6 * 4 * 4, 50)
+        self.fc2 = nn.Linear(50, 5)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = x.view(-1, 6 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return x
+
+
+net = SupvNet()
+
+summary(net, (3, 10, 10))
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
